190e5cd1537073a7e52d2841919b7b19bc4d7c62
Maxim <vyatkin.maxim@gmail.com>
2021-08-18 22:07:24 +0300

    homework 2

--- products/views.py
+++ products/views.py
@@ -1,11 +1,30 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop',
+        'time': datetime.now(),
+    }
+
+
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'GeekShop - Каталог',
+        'products': [
+            {
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6900,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни'
+            }
+        ]
+    }
+
+    return render(request, 'products/products.html', context)
