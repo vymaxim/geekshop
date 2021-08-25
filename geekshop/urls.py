680ed4cce71821181e7da54497803857451a03b0
Maxim <vyatkin.maxim@gmail.com>
2021-08-25 13:08:58 +0300

    added templates login, registration

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -23,7 +23,8 @@ from products.views import index, products
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', include('products.urls', namespace='products'))
+    path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users'))
 ]
 
 if settings.DEBUG:
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    context = {
+        'title': 'GeekShop - Авторизация'
+    }
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    context = {
+        'title': 'GeekShop - Регистрация'
+    }
+    return render(request, 'users/registration.html', context)
